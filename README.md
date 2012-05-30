5257bfa4572ae3440786a197533f78835e408911
Edward Kim <edk217@gmail.com>
2012-05-30 16:21:57 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Flickr View
+
+This is a re-write of a project I wrote a couple of months ago. [FlickrView](https://github.com/edkim/flickr_view)
+
+Requirements
+- A new user signs up using email and password. Email should be unique. Email validity and password confirmation are checked.
+- User can login using email, password
+- Each time user logs in the app greets the user and shows top 10 "interesting" flickr photos
+- Each photo is a link clickable to flickr site for the photo
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlickrView2::Application.config.secret_token = 'b4347ef084ff3fbda169c07a6ac82239fac6f99fd5abc1db1f249d884fbf9ccff08ff9610db7bb2b7ce43b4f70ca24c69e58c3de151314b4d6d7a0ea3135cf69'
